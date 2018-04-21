8df8c48dc8fcf681c95217fcc9bef42ec9289a7b
dimstar <kylermagee@gmail.com>
2018-04-21 00:11:13 -0700

    had to do some really weird stuff with my connect mysql connection ends on line 116 but having it anywhere else borks it!

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -15,16 +15,20 @@ let log = console.log;
 
 log('Welcome to Bamazon!');
 
-// GLOBAL
+// GLOBAL Cart Hack
 let Cart = {
     item_id: 0,
     product_name: '',
     stock_quantity: 0,
-    price: 0
+    price: 0,
+    checkout: false,
+    new_stock: 0
 }
 
 let getLabelVal = function(label_vals, index){
-    let label_and_val = label_vals[index].split(':');
+    // parse out the proudct id
+    let split_vals = label_vals.split(' | ');
+    let label_and_val = split_vals[index].split(':');
     return label_and_val[1];
 }
 
@@ -33,15 +37,13 @@ let storeView = [
         type: 'list',
         name: 'product',
         message: 'Please Select a Product',
-        choices: [],
+        choices: [],// these are written into the obj literal later on in makeProductChoices
         filter: function(val) {
-            // parse out the proudct id
-            let val_split = val.split(' | ');
-            // create global cart hack
-            Cart.item_id = getLabelVal(val_split, 3);
-            Cart.price = getLabelVal(val_split, 1);
-            Cart.product_name = getLabelVal(val_split, 0);
-            Cart.stock_quantity = getLabelVal(val_split, 2);
+            // set global cart hack
+            Cart.item_id = getLabelVal(val, 3);
+            Cart.price = getLabelVal(val, 1);
+            Cart.product_name = getLabelVal(val, 0);
+            Cart.stock_quantity = getLabelVal(val, 2);
             return val;
         }
     },
@@ -57,14 +59,33 @@ let storeView = [
     },
     {
         type: 'confirm',
-        name: 'check_quantity',
-        message: 'Sorry! Not enough inventory for this item.',
+        name: 'check_quantity_bad',
+        message: 'Sorry! Not enough inventory for this item. (hit enter to start over)',
         when: function(answers){
             return answers.quantity > Cart.stock_quantity;
+        }
+    },
+    {
+        type: 'confirm',
+        name: 'check_quantity_good',
+        message: 'Submit your order?',
+        when: function(answers){
+             // set global cart hack
+            Cart.checkout = true;
+            // get the current product qty in stock
+            let product_qty = getLabelVal(answers.product, 2);
+            // get the requested stock from user
+            Cart.new_stock = product_qty - answers.quantity;
+            return answers.quantity <= Cart.stock_quantity;
         }
     }
 ]
 
+/**
+ * Take in db data and formulate ui for storefront
+ * @param {*} resultPackets 
+ * @param {*} questions 
+ */
 let makeProductChoices = function(resultPackets, questions){
     for (const index in resultPackets) {
         if (resultPackets.hasOwnProperty(index)) {
@@ -74,23 +95,55 @@ let makeProductChoices = function(resultPackets, questions){
     }
     return questions;
 }
+/**
+ * Take a cart and update the database
+ * @param {*} cart 
+ */
+let updateStore = function(cart){
+        // update the database with the cart
+        if(cart.checkout === true){
+            let update_query = `UPDATE products`+
+            ` SET stock_quantity = ${cart.new_stock}`+
+            ` WHERE item_id = ${cart.item_id};`;
+            // connection.connect();
+            connection.query( update_query, function (error, results, fields) {
+                if (error) throw error;
+                if(results.changedRows === 1){
+                    console.log('Stock updated! Have a nice day!');
+                }
+                // storeCheckout(results);
+            });
+            connection.end();
+        }
+}
 
+/**
+ * Take products and display a storefront
+ * @param {*} products 
+ */
 let storeCheckout = function(products){
     storeViewComplete = makeProductChoices(products, storeView);
     Inquirer.prompt(storeViewComplete).then(answers => {
-        console.log('\nOrder receipt:');
-        console.log(JSON.stringify(answers, null, '  '));
-        log(Cart);
+        if (answers.check_quantity_bad) {
+            ask();
+          } else {
+            console.log('\nOrder receipt:');
+            console.log(JSON.stringify(answers, null, '  '));
+            // log(Cart);
+            updateStore(Cart);
+          }
+        
     });
 }//storeFront
 
-connection.connect();
+// connection.connect();
 connection.query('SELECT * FROM products', function (error, results, fields) {
     if (error) throw error;
     // console.log('The solution is: ', results);
     storeCheckout(results);
 });
-connection.end();
+// connection.end();
+
 
 // output global cart
 
